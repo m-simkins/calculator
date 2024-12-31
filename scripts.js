ebe512418ba5ba608d6a1f19d60fedea530c20e3
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-31 18:20:47 -0500

    change how buttons are generated

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
-function add(a,b) {return a + b;}
-
-function subtract(a,b) {return a - b;}
-
-function multiply(a,b) {return a * b;}
-
-function divide(a,b) {return a / b;}
-
 let a;
 let b;
 let operator;
+
+function add(a,b) {return a + b;}
+function subtract(a,b) {return a - b;}
+function multiply(a,b) {return a * b;}
+function divide(a,b) {return a / b;}
 
 function operate(a, b, operator) {
     switch (operator) {
@@ -29,49 +26,57 @@ function operate(a, b, operator) {
     }
 };
 
-createDisplay();
-createButtons();
+const inputs = "1234567890C=+-*/".split("");
+const numbers = inputs.slice(0, 9);
+const specials = inputs.slice(9, 12);
+const operators = inputs.slice(-4);
 
-function createDisplay() {
-    const display = document.createElement("div");
-    display.id = "display";
-    display.innerText = "0";
-    document.getElementById("calculator").appendChild(display);
-}
+makeButtons();
 
-function createButtons() {
-    const buttons = document.createElement("div");
-    buttons.id = "buttons";
-    const buttonValues = "1234567890+-*/".split("");
-    buttonValues.forEach(val => {
+function makeButtons() {
+    inputs.forEach((input) => {
         const button = document.createElement("button");
-        button.id = `button${val}`;
-        button.innerText = `${val}`;
-        button.value = val;
-        button.classList.add(isNaN(Number(val)) ? "operator-button" : "number-button");
-        buttons.appendChild(button);
-    });
-    document.getElementById("calculator").appendChild(buttons);
-    addSpecialButtons();
-}
-
-function addSpecialButtons() {
-    ["=", "Clear"].forEach(value => {        
-        const button = document.createElement("button");
-        button.id = `button${value}`;
-        button.innerText = `${value}`;
+        button.value = input;
+        button.id = `button${input}`;
+        button.classList.add("button");
+        button.innerText = `${input}`;
         document.getElementById("buttons").appendChild(button);
     });
 }
 
-Array.from(document.getElementsByClassName("number-button")).forEach(button => {
-    button.addEventListener("click", () => {
-        if (!a) {
-            a = Number(button.value);
-        } else {
-            b = Number(button.value);
-        }
-        document.getElementById("display").innerText = button.value;
-        console.log(a, b);
-    })
-});
+// function createButtons() {
+//     const buttons = document.createElement("div");
+//     buttons.id = "buttons";
+//     const buttonValues = "1234567890+-*/".split("");
+//     buttonValues.forEach(val => {
+//         const button = document.createElement("button");
+//         button.id = `button${val}`;
+//         button.innerText = `${val}`;
+//         button.value = val;
+//         button.classList.add(isNaN(Number(val)) ? "operator-button" : "number-button");
+//         buttons.appendChild(button);
+//     });
+//     document.getElementById("calculator").appendChild(buttons);
+//     addSpecialButtons();
+// }
+
+// function addSpecialButtons() {
+//     ["=", "Clear"].forEach(value => {        
+//         const button = document.createElement("button");
+//         button.id = `button${value}`;
+//         button.innerText = `${value}`;
+//         document.getElementById("buttons").appendChild(button);
+//     });
+// }
+
+// Array.from(document.getElementsByClassName("number-button")).forEach(button => {
+//     button.addEventListener("click", () => {
+//         if (!a) {
+//             a = Number(button.value);
+//         } else {
+//             b = Number(button.value);
+//         }
+//         document.getElementById("display").innerText = button.value;
+//         console.log(a, b);
+//     })
+// });
