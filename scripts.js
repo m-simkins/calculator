28261a88bc51472c87c018d8a1486f1b69cb930a
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-31 13:50:54 -0500

    Create initial functions (steps 1 - 3)

---
+++ scripts.js
@@ -0,0 +1,43 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+let a;
+let operator;
+let b;
+
+function operate(a, operator, b) {
+    switch (operator) {
+        case "+":
+            return add(a,b);
+            break;
+        case "-":
+            return subtract(a,b);
+            break;
+        case "*":
+            return multiply(a,b);
+            break;
+        case "/":
+            return divide(a,b);
+            break;
+        default:
+            break;
+    }
+};
+
+console.log(operate(6, "+", 3));
+console.log(operate(6, "-", 3));
+console.log(operate(6, "*", 3));
+console.log(operate(6, "/", 3));
